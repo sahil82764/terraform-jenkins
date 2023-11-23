32677a4969c4c024d309f3f7150747f15945dc7f
Sahil Khan <95177044+sahil82764@users.noreply.github.com>
2023-11-23 01:45:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,21 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "us-east-1"
 }
 
 resource "aws_instance" "foo" {
-  ami = "ami-05fa00d4c63e32376"  # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+    ami           = "ami-05fa00d4c63e32376"  # Specify the desired AMI
+    instance_type = "t2.micro"
+    tags = {
+        Name = "TF-Instance"
+    }
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update
+                sudo apt install -y python3-pip
+                git clone https://github.com/sahil82764/flask-app.git
+                cd flask-app
+                pip3 install -r requirements.txt
+                python3 app.py
+                EOF
 }
