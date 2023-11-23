ebe33c2faa945b09227193806112cb99bbd75401
Sahil Khan <95177044+sahil82764@users.noreply.github.com>
2023-11-23 01:51:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-    ami           = "ami-05fa00d4c63e32376"  # Specify the desired AMI
+    ami           = "ami-05fa00d4c63e32376" 
     instance_type = "t2.micro"
     tags = {
         Name = "TF-Instance"
@@ -13,9 +13,16 @@ resource "aws_instance" "foo" {
                 #!/bin/bash
                 sudo apt update
                 sudo apt install -y python3-pip
+
+                # Custom wait mechanism (example, use a tool like wait-for-it in production)
+                until nc -zv localhost 80; do sleep 1; done
+
                 git clone https://github.com/sahil82764/flask-app.git
                 cd flask-app
                 pip3 install -r requirements.txt
                 python3 app.py
                 EOF
+
+    # IAM role for the instance with necessary permissions (customize based on your needs)
+    iam_instance_profile = "CICD-User"
 }
